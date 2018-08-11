89a578d5f0f8b21f65acf3a99cf60dd19fb92fbd
matlegdal <matlegdal@hotmail.com>
2018-08-11 18:31:23 -0400

    reorganize files

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,10 @@
-const nodeExternals = require('webpack-node-externals')
-const resolve = (dir) => require('path').join(__dirname, dir)
+const nodeExternals = require('webpack-node-externals');
+
+const resolve = dir => require('path').join(__dirname, dir);
 
 module.exports = {
+  mode: 'spa',
+  srcDir: 'src/',
   /*
   ** Headers of the page
   */
@@ -10,16 +13,16 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'sosdyspraxie project' }
+      { hid: 'description', name: 'description', content: 'sosdyspraxie project' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
-    ]
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
+    ],
   },
   plugins: ['~/plugins/vuetify.js'],
   css: [
-    '~/assets/style/app.styl'
+    '~/assets/style/app.styl',
   ],
   /*
   ** Customize the progress bar color
@@ -32,36 +35,36 @@ module.exports = {
     babel: {
       plugins: [
         ['transform-imports', {
-          'vuetify': {
-            'transform': 'vuetify/es5/components/${member}',
-            'preventFullImport': true
-          }
-        }]
-      ]
+          vuetify: {
+            transform: 'vuetify/es5/components/${member}',
+            preventFullImport: true,
+          },
+        }],
+      ],
     },
     vendor: [
-      '~/plugins/vuetify.js'
+      '~/plugins/vuetify.js',
     ],
     extractCSS: true,
     /*
     ** Run ESLint on save
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+          exclude: /(node_modules)/,
+        });
       }
       if (ctx.isServer) {
         config.externals = [
           nodeExternals({
-            whitelist: [/^vuetify/]
-          })
-        ]
+            whitelist: [/^vuetify/],
+          }),
+        ];
       }
-    }
-  }
-}
+    },
+  },
+};
